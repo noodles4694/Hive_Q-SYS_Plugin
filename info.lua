3cf073ec2e253e4604e4a76bce93c981dc46a791
Nigel Sadler <nigel.sadler@green-hippo.com>
2025-07-24 18:16:13 +0100

    added build task and association files

--- info.lua
+++ info.lua
@@ -1,7 +1,7 @@
 PluginInfo = {
   Name = "Hive~Media Server",
   Version = "0.0",
-  BuildVersion = "0.0.0.0",
+  BuildVersion = "0.0.0.1",
   Id = "c88de492-bccb-49bb-9927-852367b9eb16",
   Author = "Carrier Labs",
   Description = "Plugin to controol Hive Media Server devices"  
