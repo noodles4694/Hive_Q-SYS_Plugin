6e96f69b1e53704af5d0b6619cd9c876e766d531
Nigel Sadler <nigel.sadler@green-hippo.com>
2025-07-24 18:31:01 +0100

    Project now builds and works correctly....here we go...

--- info.lua
+++ info.lua
@@ -1,8 +1,8 @@
 PluginInfo = {
-  Name = "Hive~Media Server",
+  Name = "Hive~Beeblade",
   Version = "0.0",
-  BuildVersion = "0.0.0.1",
+  BuildVersion = "0.0.0.3",
   Id = "c88de492-bccb-49bb-9927-852367b9eb16",
-  Author = "Carrier Labs",
-  Description = "Plugin to controol Hive Media Server devices"  
+  Author = "Carrier Labs & Hive",
+  Description = "Plugin to control Hive Beeblade, Beebox and Nexus devices"  
 }
