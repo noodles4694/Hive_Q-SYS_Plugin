f63e5bd36e6a59ee54150f3fd9dbd92e7385df26
Nigel Sadler <nigel.sadler@green-hippo.com>
2025-09-26 17:11:21 +0100

    Fixed issue with slow Modules page

--- controls.lua
+++ controls.lua
@@ -227,210 +227,209 @@ for i = 1, layer_count do
         UserPin = false
       }
     )
-
-    -- Controls for Modules Page
-    table.insert(
-      ctrls,
-      {
-        Name = "playlist_enable",
-        ControlType = "Button",
-        ButtonType = "Toggle",
-        PinStyle = "Both",
-        Count = 1,
-        UserPin = true
-      }
-    )
-    table.insert(
-      ctrls,
-      {
-        Name = "l1_timecode_enable",
-        ControlType = "Button",
-        ButtonType = "Toggle",
-        PinStyle = "Both",
-        Count = 1,
-        UserPin = true
-      }
-    )
-    table.insert(
-      ctrls,
-      {
-        Name = "l2_timecode_enable",
-        ControlType = "Button",
-        ButtonType = "Toggle",
-        PinStyle = "Both",
-        Count = 1,
-        UserPin = true
-      }
-    )
-    table.insert(
-      ctrls,
-      {
-        Name = "timeline_enable",
-        ControlType = "Button",
-        ButtonType = "Toggle",
-        PinStyle = "Both",
-        Count = 1,
-        UserPin = true
-      }
-    )
-    table.insert(
-      ctrls,
-      {
-        Name = "schedule_enable",
-        ControlType = "Button",
-        ButtonType = "Toggle",
-        PinStyle = "Both",
-        Count = 1,
-        UserPin = true
-      }
-    )
-    table.insert(
-      ctrls,
-      {
-        Name = "playlist_rows",
-        ControlType = "Knob",
-        ControlUnit = "Integer",
-        Style = "Text Field",
-        PinStyle = "Output",
-        Min = 0,
-        Max = 9999999,
-        Count = 1,
-        UserPin = true
-      }
-    )
-    table.insert(
-      ctrls,
-      {
-        Name = "playlist_current_row",
-        ControlType = "Knob",
-        ControlUnit = "Integer",
-        Style = "Text Field",
-        PinStyle = "Output",
-        Min = 0,
-        Max = 9999999,
-        Count = 1,
-        UserPin = true
-      }
-    )
-    table.insert(
-      ctrls,
-      {
-        Name = "l1_tc_rows",
-        ControlType = "Knob",
-        ControlUnit = "Integer",
-        Style = "Text Field",
-        PinStyle = "Output",
-        Min = 0,
-        Max = 9999999,
-        Count = 1,
-        UserPin = true
-      }
-    )
-    table.insert(
-      ctrls,
-      {
-        Name = "l2_tc_rows",
-        ControlType = "Knob",
-        ControlUnit = "Integer",
-        Style = "Text Field",
-        PinStyle = "Output",
-        Min = 0,
-        Max = 9999999,
-        Count = 1,
-        UserPin = true
-      }
-    )
-    table.insert(
-      ctrls,
-      {
-        Name = "playlist_play_previous",
-        ControlType = "Button",
-        ButtonType = "Trigger",
-        PinStyle = "Input",
-        Count = 1,
-        UserPin = true
-      }
-    )
-    table.insert(
-      ctrls,
-      {
-        Name = "playlist_play_next",
-        ControlType = "Button",
-        ButtonType = "Trigger",
-        PinStyle = "Input",
-        Count = 1,
-        UserPin = true
-      }
-    )
-    table.insert(
-      ctrls,
-      {
-        Name = "playlist_play_first",
-        ControlType = "Button",
-        ButtonType = "Trigger",
-        PinStyle = "Input",
-        Count = 1,
-        UserPin = true
-      }
-    )
-    table.insert(
-      ctrls,
-      {
-        Name = "playlist_play_last",
-        ControlType = "Button",
-        ButtonType = "Trigger",
-        PinStyle = "Input",
-        Count = 1,
-        UserPin = true
-      }
-    )
-    table.insert(
-      ctrls,
-      {
-        Name = "playlist_play_row",
-        ControlType = "Button",
-        ButtonType = "Trigger",
-        PinStyle = "Input",
-        Count = 1,
-        UserPin = true
-      }
-    )
-    table.insert(
-      ctrls,
-      {
-        Name = "playlist_play_row_index",
-        ControlType = "Knob",
-        ControlUnit = "Integer",
-        Style = "Text Field",
-        PinStyle = "Both",
-        Min = 1,
-        Max = 9999999,
-        DefaultValue = 1,
-        Count = 1,
-        UserPin = true
-      }
-    )
-    table.insert(
-      ctrls,
-      {
-        Name = "system_restart",
-        ControlType = "Button",
-        ButtonType = "Trigger",
-        PinStyle = "Input",
-        Count = 1,
-        UserPin = true
-      }
-    )
-    table.insert(
-      ctrls,
-      {
-        Name = "system_shutdown",
-        ControlType = "Button",
-        ButtonType = "Trigger",
-        PinStyle = "Input",
-        Count = 1,
-        UserPin = true
-      }
-    )
   end
 end
+-- Controls for Modules Page
+table.insert(
+  ctrls,
+  {
+    Name = "playlist_enable",
+    ControlType = "Button",
+    ButtonType = "Toggle",
+    PinStyle = "Both",
+    Count = 1,
+    UserPin = true
+  }
+)
+table.insert(
+  ctrls,
+  {
+    Name = "l1_timecode_enable",
+    ControlType = "Button",
+    ButtonType = "Toggle",
+    PinStyle = "Both",
+    Count = 1,
+    UserPin = true
+  }
+)
+table.insert(
+  ctrls,
+  {
+    Name = "l2_timecode_enable",
+    ControlType = "Button",
+    ButtonType = "Toggle",
+    PinStyle = "Both",
+    Count = 1,
+    UserPin = true
+  }
+)
+table.insert(
+  ctrls,
+  {
+    Name = "timeline_enable",
+    ControlType = "Button",
+    ButtonType = "Toggle",
+    PinStyle = "Both",
+    Count = 1,
+    UserPin = true
+  }
+)
+table.insert(
+  ctrls,
+  {
+    Name = "schedule_enable",
+    ControlType = "Button",
+    ButtonType = "Toggle",
+    PinStyle = "Both",
+    Count = 1,
+    UserPin = true
+  }
+)
+table.insert(
+  ctrls,
+  {
+    Name = "playlist_rows",
+    ControlType = "Knob",
+    ControlUnit = "Integer",
+    Style = "Text Field",
+    PinStyle = "Output",
+    Min = 0,
+    Max = 9999999,
+    Count = 1,
+    UserPin = true
+  }
+)
+table.insert(
+  ctrls,
+  {
+    Name = "playlist_current_row",
+    ControlType = "Knob",
+    ControlUnit = "Integer",
+    Style = "Text Field",
+    PinStyle = "Output",
+    Min = 0,
+    Max = 9999999,
+    Count = 1,
+    UserPin = true
+  }
+)
+table.insert(
+  ctrls,
+  {
+    Name = "l1_tc_rows",
+    ControlType = "Knob",
+    ControlUnit = "Integer",
+    Style = "Text Field",
+    PinStyle = "Output",
+    Min = 0,
+    Max = 9999999,
+    Count = 1,
+    UserPin = true
+  }
+)
+table.insert(
+  ctrls,
+  {
+    Name = "l2_tc_rows",
+    ControlType = "Knob",
+    ControlUnit = "Integer",
+    Style = "Text Field",
+    PinStyle = "Output",
+    Min = 0,
+    Max = 9999999,
+    Count = 1,
+    UserPin = true
+  }
+)
+table.insert(
+  ctrls,
+  {
+    Name = "playlist_play_previous",
+    ControlType = "Button",
+    ButtonType = "Trigger",
+    PinStyle = "Input",
+    Count = 1,
+    UserPin = true
+  }
+)
+table.insert(
+  ctrls,
+  {
+    Name = "playlist_play_next",
+    ControlType = "Button",
+    ButtonType = "Trigger",
+    PinStyle = "Input",
+    Count = 1,
+    UserPin = true
+  }
+)
+table.insert(
+  ctrls,
+  {
+    Name = "playlist_play_first",
+    ControlType = "Button",
+    ButtonType = "Trigger",
+    PinStyle = "Input",
+    Count = 1,
+    UserPin = true
+  }
+)
+table.insert(
+  ctrls,
+  {
+    Name = "playlist_play_last",
+    ControlType = "Button",
+    ButtonType = "Trigger",
+    PinStyle = "Input",
+    Count = 1,
+    UserPin = true
+  }
+)
+table.insert(
+  ctrls,
+  {
+    Name = "playlist_play_row",
+    ControlType = "Button",
+    ButtonType = "Trigger",
+    PinStyle = "Input",
+    Count = 1,
+    UserPin = true
+  }
+)
+table.insert(
+  ctrls,
+  {
+    Name = "playlist_play_row_index",
+    ControlType = "Knob",
+    ControlUnit = "Integer",
+    Style = "Text Field",
+    PinStyle = "Both",
+    Min = 1,
+    Max = 9999999,
+    DefaultValue = 1,
+    Count = 1,
+    UserPin = true
+  }
+)
+table.insert(
+  ctrls,
+  {
+    Name = "system_restart",
+    ControlType = "Button",
+    ButtonType = "Trigger",
+    PinStyle = "Input",
+    Count = 1,
+    UserPin = true
+  }
+)
+table.insert(
+  ctrls,
+  {
+    Name = "system_shutdown",
+    ControlType = "Button",
+    ButtonType = "Trigger",
+    PinStyle = "Input",
+    Count = 1,
+    UserPin = true
+  }
+)
